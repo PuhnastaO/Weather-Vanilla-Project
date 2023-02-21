b018a9deb2a9c10d11fff0de42550574a80c8f7b
PuhnastaO <elena4385@msn.com>
2023-02-21 14:19:22 +0100

    + city variable

--- src/app.js
+++ src/app.js
@@ -38,7 +38,6 @@ function formatDate() {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -54,5 +53,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "4aa410cf6a213ee686d681951c8186fb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+let city = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
